8ef980dd862dd3493d6737cf9caee0fb92dbc346
Сержио <sirozha16@mail.ru>
2022-11-09 22:34:35 +0300

    добавили приветствие для кати

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "катя")
+{
+    Console.WriteLine("Ура, это же КАТЯ!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
